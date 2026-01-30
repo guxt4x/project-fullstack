314163fb16e6e6ded238def7f22784884e0a8556
guxt4x <061guxta@gmail.com>
2026-01-29 22:44:22 -0300

    feat: implement login functionality with event emitters and service integration

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -3,6 +3,7 @@ import { DefaultLoginLayout } from '../../components/default-login-layout/defaul
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { email } from '@angular/forms/signals';
 import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,16 +12,33 @@ import { PrimaryInput } from '../../components/primary-input/primary-input';
     ReactiveFormsModule,
     PrimaryInput,
   ],
+  providers: [
+    Login
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router
+    private loginService: Login,
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     });
+  }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => console.log("Sucesso"),
+      error: () => console.log("Sucesso")
+    })
+  }
+
+  navigate() {
+    this.router.navigate(["signup"])
   }
 }
---
+++ src/app/services/login.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-responde.type';
+import { __values } from 'tslib';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Login {
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(name: string, password: string) {
+    return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("Auth-Token", value.token)
+        sessionStorage.setItem("Username", value.name)
+      }
+      )
+  }
+}
