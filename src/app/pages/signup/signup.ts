d86412fa526427e4df2aa4c75636e3bf4c51b679
guxt4x <88469502+guxt4x@users.noreply.github.com>
2026-04-29 04:37:24 -0300

    feat: signup component

---
+++ src/app/pages/signup/signup.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayout } from '../../components/default-login-layout/default-login-layout';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInput } from '../../components/primary-input/primary-input';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-signup',
+  imports: [
+    DefaultLoginLayout,
+    ReactiveFormsModule,
+    PrimaryInput,
+  ],
+  providers: [
+    LoginService
+  ],
+  templateUrl: './signup.html',
+  styleUrl: './signup.scss',
+})
+export class Signup {
+  signupForm!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    });
+  }
+
+  submit() {
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login realizado com sucesso!"),
+      error: () => this.toastrService.error("Erro inesperado ao realizar login!")
+    })
+  }
+
+  navigate() {
+    this.router.navigate(["login"])
+  }
+}
